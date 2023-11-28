0a05e50836674aa1270d6428ecd97f42a75dc3d3
Bruno Rocha <mail.brunorocha@gmail.com>
2023-11-28 13:23:47 -0300

    Initial Commit

---
+++ Scriptura/ScripturaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScripturaApp.swift
+//  Scriptura
+//
+//  Created by Bruno Rocha on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ScripturaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
